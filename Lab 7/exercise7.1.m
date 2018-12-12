4ed4dd1cfa37cad6ad0649d8b903e1bd11ea92fc
Henrique Novais <henriquennoliveira@gmail.com>
2018-12-11 23:24:11 -0300

    Implement image dilation algorithm

---
+++ Lab 7/exercise7.1.m
@@ -0,0 +1,21 @@
+%Dilate image with structuring element SE
+
+BW1 = imread('circulos.tif');
+subplot(2,3,1), imshow(BW1), title('Original Image');
+
+BW2 = BW1;
+SE = [0 0 1 1 0 0;
+      0 0 1 1 0 0;
+      1 1 1 1 1 1;
+      1 1 1 1 1 1;
+      0 0 1 1 0 0;
+      0 0 1 1 0 0];
+
+for i= 1:5
+  C = imdilate(BW2, SE);
+  name = ['Dilated image ', int2str(i)];
+  subplot(2,3,i+1), imshow(C), title(name);
+  name = [name, '.png'];
+  imwrite(C, name);
+  BW2 = C;
+  end      
